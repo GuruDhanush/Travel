d3deb8a93fe98ded0615852e4ec21f5197281d91
Guru Dhanush <22199598+GuruDhanush@users.noreply.github.com>
2019-02-06 17:38:07 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// 'use strict';
+// let itemList = ["Hyderabad", "Chennai", "Benguluru", "Delhi", "Amaravathi"];
+// let fromInput = document.getElementsByTagName("input");
+// console.log(fromInput,);
+
+// fromInput.oninput = function(event)  {
+
+//     let d = document.createElement("div");
+
+//     console.log(event);
+
+// };
